ba2e538a28815f31f52d8802e5f1cc0e0dbb3a98
tjklein <TJKlein@gmail.com>
2022-03-23 21:12:48 +0000

    added data

---
+++ data/download_wiki.sh
@@ -0,0 +1,6 @@
+#
+# SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+wget https://huggingface.co/datasets/princeton-nlp/datasets-for-simcse/resolve/main/wiki1m_for_simcse.txt
